93ab73be7f4675e05e08bbffc5599a7356026a49
MagicTearsAsunder <The.Magic.Tears.Asunder@protonmail.com>
2019-11-11 22:49:00 +0300

    added classes inspector

---
+++ classes_introspector.py
@@ -0,0 +1,27 @@
+def inspector(instance):
+    print("Instance attributes:")
+
+    for i in sorted(instance.__dict__.keys()):
+        print(f"{i}: {instance.__dict__[i]}")
+
+    print("-" * 65)
+    print(" Attributes    | Value")
+    cache = set()
+    recsearch(instance.__class__, cache)
+
+
+def recsearch(the_class, cache):
+    print("-" * 65)
+    cache.add(the_class)
+
+    for i in sorted(the_class.__dict__.keys()):
+        placeholder = " " * (15 - len(i))
+        print(f"{i}{placeholder}| {the_class.__dict__[i]}")
+
+    if the_class.__bases__ == (object,):
+        print("-" * 65)
+        print("Max superclasses origin depth was reached.\n")
+    else:
+        for i in the_class.__bases__:
+            if i not in cache:
+                recsearch(i, cache)
