c5f0ae4a64066316b85981fbb9dedfad69ee67fb
MagicTearsAsunder <The.Magic.Tears.Asunder@protonmail.com>
2020-04-17 21:43:49 +0300

    Added class_decorator

---
+++ class_decorator.py
@@ -0,0 +1,58 @@
+import time
+from functools import wraps
+
+
+def decorated(method):
+    method._decorated = True
+    return method
+
+
+def method_decorator(method, cache):
+    @wraps(method)
+    def new_method(self):
+        if self.value not in cache[method.__name__]:
+            if len(cache[method.__name__]) > 3:
+                cache[method.__name__].pop(next(iter(cache[method.__name__])))
+            cache[method.__name__][self.value] = method(self)
+        return cache[method.__name__][self.value]
+    return new_method
+
+
+def cache(cache_name):
+    cache_name = cache_name or "CACHE"
+
+    def class_decorator(cls):
+        setattr(cls, cache_name, {})
+        cache = getattr(cls, cache_name)
+        for method_name in cls.__dict__:
+            method = getattr(cls, method_name)
+            if hasattr(method, '_decorated'):
+                cache[method_name] = {}
+                setattr(cls, method_name, method_decorator(method, cache))
+        return cls
+    return class_decorator
+
+
+@cache(cache_name="MY_CACHE_NAME")
+class Counter:
+    def __init__(self, value):
+        self.value = value
+
+    @decorated
+    def sqr(self):
+        print("Calculating..")
+        time.sleep(1)
+        return type(self)(self.value**2)
+
+    def sqrt(self):
+        print("Calculating..")
+        time.sleep(1)
+        return type(self)(self.value**0.5)
+
+    def half(self):
+        print("Calculating..")
+        time.sleep(1)
+        return type(self)(self.value // 2)
+
+    def __repr__(self):
+        return f"Value is {self.value}"
